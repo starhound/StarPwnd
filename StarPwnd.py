a250ab78c655f1cb689e6bb457d98a4d51cf017f
Wes <wesleyreid0@gmail.com>
2020-05-06 19:41:35 -0400

    Main script

---
+++ StarPwnd.py
@@ -0,0 +1,36 @@
+import requests
+import hashlib
+
+password = input("Please enter your password: ")
+
+result = hashlib.sha1(password.encode('utf-8'))
+
+print("SHA1 Hash: ")
+print(result.hexdigest())
+print("\n\n")
+print("Password API Results\n")
+
+hashedChars = result.hexdigest()[:5]
+resp = requests.get('https://api.pwnedpasswords.com/range/' + hashedChars)
+content = resp.text
+lines = content.splitlines()
+passwordList = []
+
+for line in lines:
+    head, sep, tail = line.partition(':')
+    returnedPass = hashedChars.upper() + head.upper()
+    passwordList.append(returnedPass)
+    print(returnedPass + " " + sep + tail)
+
+hashedPass = result.hexdigest().upper()
+foundPass = 0
+
+for password in passwordList:
+    if password == hashedPass:
+        foundPass = password
+        break
+
+if foundPass:
+    print("Password Found: " + foundPass)
+else:
+    print("Password not found.")
